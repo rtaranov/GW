0da7edb6abb8efc1366e78e916cf8321da243154
Roman Taranov <r.taranov@gmail.com>
2013-06-14 00:34:22 +0300

    2 test

--- trunk/netserver.c
+++ trunk/netserver.c
@@ -1,12 +1,4 @@
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <strings.h>
-#include <sys/types.h> 
-#include <sys/socket.h>
-#include <netinet/in.h>
-*/
+
 	#include <stdio.h>
 	#include <stdlib.h>
 	#include <pthread.h>
